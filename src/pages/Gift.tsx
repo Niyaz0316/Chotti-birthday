fd6107a7df6f8acd911364d500a03633f0f93c2f
Niyaz0316 <149532544+Niyaz0316@users.noreply.github.com>
2025-04-20 11:09:30 +0530

    Update Gift.tsx

--- src/pages/Gift.tsx
+++ src/pages/Gift.tsx
@@ -29,7 +29,7 @@ const Gift = () => {
           </h1>
           <p className="text-lg md:text-xl text-pink-200 max-w-2xl mx-auto leading-relaxed mb-8">
             “ Many more happy returns of the day Chotti!! 🥳. May your day be filled with joy 🤩, laughter, and all the love in the world!. Here's to another fabulous year ahead! Enjoy your special day to the fullest!. 
-            Aur party tho banta h😉 “
+                                         Aur party tho banta h😉 “
           </p>
         </motion.div>
       )}
